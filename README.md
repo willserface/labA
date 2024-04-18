fe3fd2c931384875643173a73e50641c5d892b25
willserface <will@serface.com>
2024-04-18 18:30:01 -0400

    Completed Lab
    Coded implementations for Trie structure, along with the insert, numberOfOccurrences, deallocateTrie, createTrie and readDictionary functions. Updated README with instructions for custom input file mode

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# labA
- 
+# Lab Assignment 0xA (Lab 10)
+##### Implemented by: Will Serface
+##### Assigned by: Prof. N. Torosdagli
+##### Course: COP3502C.0012
+##### Due Date: 18 April 2024
+
+---
+
+## Initial Commit contains Project Files provided by Assignment
+- Project Code: [`lab10.c`](./lab10.c)
+- Input File: [`dictionary.txt`](./dictionary.txt)
+
+Later Commits implement project structures and functions
+
+---
+
+## Specific Build Instructions
+
+After compiling [`lab10.c`](./lab10.c), the resulting executable is designed to run
+using the following inputs.
+
+### Default Mode (No Arguments)
+
+###### Console Command: `lab10.exe`
+
+If the executable is given no arguments upon runtime, it will assume there is a copy of 
+the input file named [`dictionary.txt`](./dictionary.txt) in the working directory.
+The executable will read its contents, and output the program results to the console.
+
+### Custom Input Mode (1 Argument)
+
+###### Console Command: `lab10.exe "dictionary.txt"`
+
+If the executable is given one argument upon runtime, it will proceed with the input
+sequence, but using the name of the file provided by the argument.
+The executable will read its contents, and output the program results to the console.
--- lab10.c
+++ lab10.c
@@ -2,61 +2,108 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define ALPHABET 26 // Length of Alphabet
+
 // Trie structure
-struct Trie
-{
+struct Trie {
+    char letter, // Current Branch's Letter
+    occurrences; // Number of Occurrences where Node was last letter
+    struct Trie *branch[ALPHABET]; // Trie Branches
 };
 
 // Inserts the word to the trie structure
-void insert(struct Trie *pTrie, char *word)
-{
+void insert(struct Trie *pTrie, char *word) {
+    if (pTrie == NULL) return; // Check for NULL
+    struct Trie *current = pTrie; // Pointer to current Node starting at pTrie
+    while (strlen(word)) { // Repeat until end of Word
+        if (!(current->branch[word[0] - 'a'])) {
+            current->branch[word[0] - 'a'] = calloc(1, sizeof(struct Trie)); // Allocate branch
+            current->branch[word[0] - 'a']->letter = word[0]; // Set new branch's letter
+        }
+        current = current->branch[word[0] - 'a']; // Increment Branch
+        word++; // Increment Character in Word
+    }
+    current->occurrences++; // Increment Occurrences if end of Word
 }
 
-// computes the number of occurances of the word
-int numberOfOccurances(struct Trie *pTrie, char *word)
-{
+// computes the number of occurrences of the word
+int numberOfOccurrences(struct Trie *pTrie, char *word) {
+    struct Trie *current = pTrie; // Pointer to current node starting at pTrie
+    while (strlen(word)) { // Repeat until end of Word
+        current = current->branch[word[0] - 'a']; // Increment Node
+        if (current) word++; // Increment Character in Word if current Node exists
+        else return 0; // Current Node does not exist
+    }
+    return current->occurrences; // Return occurrences of current Node
 }
 
 // deallocate the trie structure
-struct Trie *deallocateTrie(struct Trie *pTrie)
-{
+struct Trie *deallocateTrie(struct Trie *pTrie) {
+    if (pTrie) { // Check if Trie exists
+        for (int i = 0; i < ALPHABET; ++i) { // Loop through all Branches
+            if (pTrie->branch[i]) // Check if Branch exists
+                pTrie->branch[i] = deallocateTrie(pTrie->branch[i]); // Deallocate and set Branch to NULL
+        }
+        free(pTrie); // Deallocate current branch
+    }
+    return NULL; // Returns NULL always
 }
 
 // Initializes a trie structure
-struct Trie *createTrie()
-{
+struct Trie *createTrie() {
+    struct Trie *out = calloc(1, sizeof(struct Trie)); // Allocate Node
+    out->letter = '\n'; // Set Trie root Node to NEWLINE
+    return out; // Return Node
 }
 
 // this function will return number of words in the dictionary,
 // and read all the words in the dictionary to the structure words
-int readDictionary(char *filename, char **pInWords)
-{
+int readDictionary(char *filename, char **pInWords) {
+    FILE *input = fopen(filename, "r"); // Open Input File for Reading
+    int length; // Length of Input File
+    fscanf(input, "%d", &length); // Read Length of Input File from first line
+    for (int i = 0; i < length; ++i) { // Read remainder of Input File
+        char *word = calloc(256, 1); // Allocate 256-character word to all NULL terminator characters
+        fscanf(input, "%255s", word); // Read Word from Input File
+        pInWords[i] = word; // Save to index of Input Words
+    }
+    return length; // Return number of inputs
 }
 
-int main(void)
-{
+// GLOBAL VARIABLES
+char *dictionary; // File path of input dictionary
+
+int main(int nArgs, char **args) {
+    switch (nArgs) { // Determine Operating Schema for I/O (Input/Output Mode)
+        case 2: // 2 Arguments: Working Directory, Input File Path (Input Mode)
+            dictionary = args[1]; // Set Dictionary to custom input
+            break; // Prevent further execution
+        case 1: // 1 Argument: Working Directory (Default Mode)
+            dictionary = "dictionary.txt"; // Set Dictionary to default input
+            break; // Prevent further execution
+        default: // Not Expected
+            exit(1); // Exit with Error Code 1
+    }
+
     char *inWords[256];
 
     //read the number of the words in the dictionary
-    int numWords = readDictionary("dictionary.txt", inWords);
-    for (int i=0;i<numWords;++i)
-    {
-        printf("%s\n",inWords[i]);
+    int numWords = readDictionary(dictionary, inWords);
+    for (int i = 0; i < numWords; ++i) {
+        printf("%s\n", inWords[i]);
     }
 
     struct Trie *pTrie = createTrie();
-    for (int i=0;i<numWords;i++)
-    {
+    for (int i = 0; i < numWords; i++) {
         insert(pTrie, inWords[i]);
     }
-    // parse lineby line, and insert each word to the trie data structure
+    // parse line by line, and insert each word to the trie data structure
     char *pWords[] = {"notaword", "ucf", "no", "note", "corg"};
-    for (int i=0;i<5;i++)
-    {
-        printf("\t%s : %d\n", pWords[i], numberOfOccurances(pTrie, pWords[i]));
+    for (int i = 0; i < 5; i++) {
+        printf("\t%s : %d\n", pWords[i], numberOfOccurrences(pTrie, pWords[i]));
     }
     pTrie = deallocateTrie(pTrie);
-    if (pTrie != NULL)
-        printf("There is an error in this program\n");
+    if (pTrie != NULL) printf("There is an error in this program\n");
+    for (int i = 0; i < numWords; ++i) free(inWords[i]); // Free Input Words List
     return 0;
 }
